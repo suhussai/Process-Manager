7c338f65e47495766dd67e409cec424194d834bf
Syed Hussain <suhussai@cs.ualberta.ca>
2015-11-25 13:48:32 -0700

    trying to implement select function call

--- server.c
+++ server.c
@@ -154,9 +154,57 @@ int readFromClient() {
     free(logMessage);
     return 1;
   }
-
+  
 
 }
+
+int readFromClient2(struct con *cp) {
+  // returns 1 meaning send process name and lifespan
+  // else do nothing
+
+
+  // check for messages from client
+  char * logMessage = malloc(200);
+  int w2 = read (cp->sd, logMessage, 200);
+  //  close(snew);
+  while (w2 < 0) {
+    w2 = read (snew, logMessage, 200);
+    printf("trying to read again \n");
+    sleep(5);
+  }
+  debugPrint("we got %s from client \n", logMessage);
+
+
+  if (logMessage[0] == '[') {
+    debugPrint("request for logging received \n");
+    FILE *logFile;
+    logFile = fopen(getenv("PROCNANNYLOGS"), "a");    
+
+    debugPrint("logging this: %s \n", logMessage);    
+    char * newLogMessage = trimwhitespace(logMessage);
+    fputs(newLogMessage, logFile);
+    fputs("\n",logFile);
+    fclose(logFile);
+    free(logMessage);
+    return 0;
+  }
+  else if (logMessage[0] == 'k') {
+    logMessage[0] = '0';
+    debugPrint("updating kill count: %s\n", logMessage);
+    totalProcsKilled += atoi(logMessage);
+    debugPrint("new kill count %d\n", totalProcsKilled);
+    free(logMessage);
+    return 0;
+  }
+
+  else {
+    free(logMessage);
+    return 1;
+  }
+  
+
+}
+
 
 
 
@@ -168,10 +216,163 @@ int readFromClient() {
 /* chines.							         */
 /* --------------------------------------------------------------------- */
 
+
+
+/*
+ * Copyright (c) 2008 Bob Beck <beck@obtuse.com>
+ * Some changes (related to the port number) by ***REMOVED***, March 2011.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
+ * select_server.c - an example of using select to implement a non-forking
+ * server. In this case this is an "echo" server - it simply reads
+ * input from clients, and echoes it back again to them, one line at
+ * a time.
+ *
+ * to use, cc -DDEBUG -o select_server select_server.c
+ * or cc -o select_server select_server.c after you read the code :)
+ *
+ * then run with select_server PORT
+ * where PORT is some numeric port you want to listen on.
+ * (***REMOVED***:  You can also get the OS to choose the port by not specifying PORT)
+ *
+ * to connect to it, then use telnet or nc
+ * i.e.
+ * telnet localhost PORT
+ * or
+ * nc localhost PORT
+ * 
+ */
+
+
+#include <sys/param.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+
+#include <netinet/in.h>
+
+#include <err.h>
+#include <errno.h>
+#include <limits.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/* we use this structure to keep track of each connection to us */
+struct con {
+	int sd; 	/* the socket for this connection */
+	int state; 	/* the state of the connection */
+	struct sockaddr_in sa; /* the sockaddr of the connection */
+	size_t  slen;   /* the sockaddr length of the connection */
+	char *buf;	/* a buffer to store the characters read in */
+	char *bp;	/* where we are in the buffer */
+	size_t bs;	/* total size of the buffer */
+	size_t bl;	/* how much we have left to read/write */
+};
+
+#define MAXCONN 2
+struct con connections[2]; 
+#define BUF_SIZE 63
+#define	MY_PORT	2222
+
+/* states used in struct con. */
+#define STATE_UNUSED 0
+#define STATE_READING 1
+#define STATE_WRITING 2
+
+
+
+/* --------------------------------------------------------------------- */
+/* This	 is  a sample server which opens a stream socket and then awaits */
+/* requests coming from client processes. In response for a request, the */
+/* server sends an integer number  such	 that  different  processes  get */
+/* distinct numbers. The server and the clients may run on different ma- */
+/* chines.							         */
+/* --------------------------------------------------------------------- */
+
+
+
+
+
+/*
+ * get a free connection structure, to save a new connection in
+ */
+struct con * get_free_conn()
+{
+	int i;
+	for (i = 0; i < MAXCONN; i++) {
+		if (connections[i].state == STATE_UNUSED)
+			return(&connections[i]);
+	}
+	return(NULL); /* we're all full - indicate this to our caller */
+}
+
+
+
+/*
+ * close or initialize a connection - resets a connection to the default,
+ * unused state.
+ */
+void closecon (struct con *cp, int initflag)
+{
+	if (!initflag) {
+		if (cp->sd != -1)
+			close(cp->sd); /* close the socket */
+		free(cp->buf); /* free up our buffer */
+	}
+	memset(cp, 0, sizeof(struct con)); /* zero out the con struct */
+	cp->buf = NULL; /* unnecessary because of memset above, but put here
+			 * to remind you NULL is 0.
+			 */
+	cp->sd = -1;
+}
+
+/* ***REMOVED*** */
+/*
+ * getPortNumber - given a valid file descriptor/socket, return the port number
+ */
+int getPortNumber( int socketNum )
+{
+	struct sockaddr_in addr;
+	int rval;
+	socklen_t addrLen;
+
+	addrLen = (socklen_t)sizeof( addr );
+
+	/* Use getsockname() to get the details about the socket */
+	rval = getsockname( socketNum, (struct sockaddr*)&addr, &addrLen );
+	if( rval != 0 )
+		err(1, "getsockname() failed in getPortNumber()");
+
+	/* Note cast and the use of ntohs() */
+	return( (int) ntohs( addr.sin_port ) );
+} /* getPortNumber */
+
+
+
 int main(int argc, char * argv[]) {
   debugPrint("starting program\n");
   clearLogs();
   writeParentPIDToLogs();
+  int max = -1, omax;
+
+  fd_set *readable = NULL;
+      
 
   sock = socket (AF_INET, SOCK_STREAM, 0);
   if (sock < 0) {
@@ -223,7 +424,11 @@ int main(int argc, char * argv[]) {
 
   char delimiter = ' ';
   char * tmpStr = malloc(63);
-
+  int i = 0;
+  for (i = 0; i < MAXCONN; i++) {
+    closecon(&connections[i], 1);
+  }
+  
   while(rereadFromConfigFile == 1) {
 
 
@@ -267,6 +472,140 @@ int main(int argc, char * argv[]) {
 
     rereadFromConfigFile = -1;
     while(keepLooping == 1) {
+
+      int i = 0;
+      int maxfd = -1;
+      omax = max;
+      max = sock;
+      
+      for (i = 0; i < MAXCONN; i++) {
+	if (connections[i].sd > max) {
+	  max = connections[i].sd;
+	}	
+      }
+      
+      if (max > omax) {
+	/* we need bigger fd_sets allocated */
+
+	/* free the old ones - does nothing if they are NULL */
+	free(readable);
+
+	/*
+	 * this is how to allocate fd_sets for select
+	 */
+	readable = (fd_set *)calloc(howmany(max + 1, NFDBITS),
+				    sizeof(fd_mask));
+	if (readable == NULL)
+	  err(1, "out of memory");
+	omax = max;
+      } else {
+	/*
+	 * our allocated sets are big enough, just make
+	 * sure they are cleared to 0. 
+	 */
+	memset(readable, 0, howmany(max+1, NFDBITS) *
+	       sizeof(fd_mask));
+	memset(writable, 0, howmany(max+1, NFDBITS) *
+	       sizeof(fd_mask));
+      }
+
+
+      FD_SET(sd, readable);
+      if (maxfd < sd)
+	maxfd = sd;
+
+      /*
+       * now go through the list of connections, and if we
+       * are interested in reading from, or writing to, the
+       * connection's socket, put it in the readable, or
+       * writable fd_set - in preparation to call select
+       * to tell us which ones we can read and write to.
+       */
+      for (i = 0; i<MAXCONN; i++) {
+	if (connections[i].state == STATE_READING) {
+	  FD_SET(connections[i].sd, readable);
+	  if (maxfd < connections[i].sd)
+	    maxfd = connections[i].sd;
+	}
+      }
+
+      i = select(maxfd + 1, readable, writable, NULL,NULL);
+      if (i == -1  && errno != EINTR)
+	err(1, "select failed");
+      if (i > 0) {
+
+	if (FD_ISSET(sd, readable)) {
+	  struct con *cp;
+	  int newsd;
+	  socklen_t slen;
+	  struct sockaddr_in sa;
+
+	  slen = sizeof(sa);
+	  newsd = accept(sd, (struct sockaddr *)&sa,
+			 &slen);
+	  if (newsd == -1)
+	    err(1, "accept failed");
+
+	  cp = get_free_conn();
+	  if (cp == NULL) {
+	    /*
+	     * we have no connection structures
+	     * so we close connection to our
+	     * client to not leave him hanging
+	     * because we are too busy to
+	     * service his request
+	     */
+	    close(newsd);
+	  } else {
+	    /*
+	     * ok, if this worked, we now have a
+	     * new connection. set him up to be
+	     * READING so we do something with him
+	     */
+	    cp->state = STATE_READING;
+	    cp->sd = newsd;
+	    cp->slen = slen;
+	    memcpy(&cp->sa, &sa, sizeof(sa));
+	  }
+	}
+	for (i = 0; i<MAXCONN; i++) {
+	  if ((connections[i].state == STATE_READING) &&
+	      FD_ISSET(connections[i].sd, readable)) {
+	    // something is readable
+	    if (readFromClient2(&connections[i]) == 1) {
+	      debugPrint("request for info received... sending\n");
+	      debugPrint("beginning write to internet socket\n");
+	      snprintf(tmpStr, 63, "%-20s#%20d!%20d", "null", getSize(processList), 5);
+	      debugPrint("writing this to pipe: %s \n", tmpStr);
+	      writeToClient(tmpStr);
+	      sleep(0.5);
+	      struct node * currentNode = processList;
+	      while (currentNode != NULL) {
+	  
+		snprintf(tmpStr, 63, "%-20s#%20d!%20d", currentNode->value, 11, currentNode->key);
+		debugPrint("writing this to pipe: %s \n", tmpStr);
+		writeToClient(tmpStr);	  
+		currentNode = currentNode->next;
+	      }
+      
+  
+	      debugPrint("finished writing to internet socket\n");
+	      close (snew);
+	      //fclose(fp2);
+
+
+	    }
+
+	  }
+
+	}
+
+      }
+
+
+
+      
+      
       if (readFromClient() == 1) {
 	debugPrint("request for info received... sending\n");
 	debugPrint("beginning write to internet socket\n");
